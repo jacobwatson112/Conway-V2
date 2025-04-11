85ed88d0074300ac0ad2ac9b84e02fa2aaf344d0
Jacob Watson <jacobwatson112@gmail.com>
2025-04-12 11:15:45 +1200

    Added admin functionality

--- src/helpers/user-helper.js
+++ src/helpers/user-helper.js
@@ -1,10 +1,28 @@
-import usersJSON from "../json/users.json" assert { type: 'json'}
+import path from "path";
+import fs from "fs";
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const databasePath = path.join(__dirname, "../json/users.json");
 
 export function isUser(messageUserId) {
-    return getUser(usersJSON.users, messageUserId)
+    return getUser(messageUserId)
 }
 
-export function getUser(users, messageUserId) {
+export function isAdminUser(messageUserId) {
+    const user = getUser(messageUserId)
+    return user?.admin
+}
+
+export function getUser(messageUserId) {
+    let users
+    if (fs.existsSync(databasePath)) {
+        const data = JSON.parse(fs.readFileSync(databasePath, "utf8"));
+        users = data.users
+    } else {
+        console.log('Error: Failed to get users')
+    }
+
     for (let user of users) {
         if (user.id === messageUserId) {
             return user
